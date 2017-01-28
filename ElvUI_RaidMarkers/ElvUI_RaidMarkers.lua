d88d745d22a49a02e8ae2712fa69ef36902019a3
Appolyonn <jellefenxd@gmail.com>
2017-01-28 16:37:47 +0200

    ElvUI_RaidMarkers

---
+++ ElvUI_RaidMarkers/ElvUI_RaidMarkers.lua
@@ -0,0 +1,224 @@
+local E, L, V, P, G = unpack(ElvUI);
+local RM = E:NewModule("RaidMarkersBar")
+local EP = LibStub("LibElvUIPlugin-1.0")
+
+local _G = _G
+local ipairs = ipairs;
+local format = string.format;
+
+-- Profile
+P["actionbar"]["raidmarkersbar"] = {
+	["visible"] = "auto",
+	["orient"] = "horizontal",
+	["scale"] = 1.0
+}
+
+-- Config
+local function InjectOptions()
+	E.Options.args.actionbar.args.raidmarkersbar = {
+		type = "group",
+		name = L["Raid Markers"],
+		args = {
+			header = {
+				order = 1,
+				type = "header",
+				name = L["Raid Markers"]
+			},
+			visible = {
+				order = 2,
+				type = "select",
+				name = L["Visibility"],
+				desc = L["Select how the raid markers bar will be displayed."],
+				values = {
+					["hide"] = L["Hide"],
+					["show"] = L["Show"],
+					["auto"] = L["Automatic"]
+				},
+				get = function() return RM.db.visible end,
+				set = function(_, value) RM.db.visible = value; RM:UpdateBar(); end
+			},
+			orient = {
+				order = 3,
+				type = "select",
+				name = L["Orientation"],
+				desc = L["Choose the orientation of the raid markers bar."],
+				values = {
+					["horizontal"] = L["Horizontal"],
+					["vertical"] = L["Vertical"]
+				},
+				get = function() return RM.db.orient end,
+				set = function(_, value) RM.db.orient = value; RM:UpdateBar(); end
+			},
+			scale = {
+				order = 4,
+				type = "range",
+				name = L["Scale"],
+				get = function() return RM.db.scale end,
+				set = function(_, value) RM.db.scale = value; RM:UpdateBar(); RM:UpdateMover(); end,
+				min = 0.5, max = 5.0, step = 0.1
+			}
+		}
+	}
+end
+
+local db
+local BUTTON_HEIGHT = 18
+local BUTTON_WIDTH = 18
+local BUTTON_DISTANCE = 5
+local FRAME_HEIGHT = 22
+local FRAME_WIDTH = 150
+
+local buttonMap = {
+	[1] = {RT = 1},	-- yellow/star
+	[2] = {RT = 2},	-- orange/circle
+	[3] = {RT = 3},	-- purple/diamond
+	[4] = {RT = 4},	-- green/triangle
+	[5] = {RT = 5},	-- white/moon
+	[6] = {RT = 6},	-- blue/square
+	[7] = {RT = 7},	-- red/cross
+	[8] = {RT = 8},	-- white/skull
+	[9] = {RT = 0}	-- clear target
+}
+
+function RM:ToggleBar()
+	if(self.db.show) then
+		self.frame:Show()
+	else
+		self.frame:Hide()
+	end
+end
+
+function RM:UpdateMover()
+	local scale = self.frame:GetScale()
+
+	self.frame.mover:Size(self.frame:GetWidth() * scale, self.frame:GetHeight() * scale)
+end
+
+function RM:UpdateBar(first)
+	local height, width = FRAME_HEIGHT, FRAME_WIDTH
+
+	if(self.db.orient == "vertical") then
+		width = BUTTON_WIDTH + 3
+		height = (BUTTON_HEIGHT * 9) + (BUTTON_DISTANCE * 9)
+	else
+		width = (BUTTON_WIDTH * 9) + (BUTTON_DISTANCE * 9)
+		height = BUTTON_HEIGHT + 3
+	end
+
+	if(first) then
+		self.frame:ClearAllPoints()
+		self.frame:SetPoint("CENTER")
+	end
+
+	self.frame:SetScale(self.db.scale or 1.0)
+	self.frame:SetWidth(width)
+	self.frame:SetHeight(height)
+
+	for i = 9, 1, -1 do
+		local button = self.frame.buttons[i]
+		local prev = self.frame.buttons[i + 1]
+		button:ClearAllPoints()
+
+		if(self.db.orient == "vertical") then
+			if(i == 9) then
+				button:SetPoint("TOP", 0, -3)
+			else
+				button:SetPoint("TOP", prev, "BOTTOM", 0, -BUTTON_DISTANCE)
+			end
+		else
+			if(i == 9) then
+				button:SetPoint("LEFT", 3, 0)
+			else
+				button:SetPoint("LEFT", prev, "RIGHT", BUTTON_DISTANCE, 0)
+			end
+		end
+	end
+
+	if(self.db.visible == "hide") then
+		UnregisterStateDriver(self.frame, "visibility")
+		if(self.frame:IsShown()) then
+			self.frame:Hide()
+		end
+	elseif(self.db.visible == "show") then
+		UnregisterStateDriver(self.frame, "visibility")
+		if(not self.frame:IsShown()) then
+			self.frame:Show()
+		end
+	else
+		RegisterStateDriver(self.frame, "visibility", "[noexists,nogroup] hide; show")
+	end
+end
+
+function RM:ButtonFactory()
+	for i, buttonData in ipairs(buttonMap) do
+		local button = CreateFrame("Button", ("ElvUI_RaidMarkersBarButton%d"):format(i), _G["ElvUI_RaidMarkersBar"], "SecureActionButtonTemplate")
+		button:SetHeight(BUTTON_HEIGHT)
+		button:SetWidth(BUTTON_WIDTH)
+		button:CreateBackdrop("Default", true)
+
+		local image = button:CreateTexture(nil, "BACKGROUND")
+		image:SetAllPoints()
+		image:SetTexture(i == 9 and "Interface\\BUTTONS\\UI-GroupLoot-Pass-Up" or ("Interface\\TargetingFrame\\UI-RaidTargetingIcon_%d"):format(i))
+
+		local highlight = button:CreateTexture(nil, "HIGHLIGHT")
+		highlight:SetAllPoints(image)
+		highlight:SetTexture(1, 1, 1, 0.30)
+
+		local target = buttonData.RT
+
+		if(target) then
+			button:SetAttribute("type1", "macro")
+			button:SetAttribute("macrotext1", ("/run SetRaidTargetIcon(\"target\", %d)"):format(i < 9 and i or 0))
+
+			button:SetScript("OnEnter", function(self)
+				GameTooltip:SetOwner(self, "ANCHOR_BOTTOM")
+				GameTooltip:SetText(L["ElvUI Raid Markers"])
+				GameTooltip:AddLine(i == 9 and L["Click to clear the mark."] or L["Click to mark the target."], 1, 1, 1)
+				GameTooltip:Show()
+			end)
+			button:SetScript("OnLeave", function() GameTooltip:Hide() end)
+		end
+
+		button:RegisterForClicks("AnyDown")
+		self.frame.buttons[i] = button
+	end
+end
+
+function RM:InitBar()
+	self.frame:ClearAllPoints()
+	self.frame:SetPoint("CENTER")
+
+	local height, width = FRAME_HEIGHT, FRAME_WIDTH
+
+	if(self.db.orient == "vertical") then
+		width = BUTTON_WIDTH + 3
+		height = (BUTTON_HEIGHT * 9) + (BUTTON_DISTANCE * 9)
+	else
+		width = (BUTTON_WIDTH * 9) + (BUTTON_DISTANCE * 9)
+		height = BUTTON_HEIGHT + 3
+	end
+
+	self.frame:SetWidth(width)
+	self.frame:SetHeight(height)
+end
+
+function RM:Initialize()
+	self.db = E.db.actionbar.raidmarkersbar
+
+	self.frame = CreateFrame("Frame", "ElvUI_RaidMarkersBar", E.UIParent, "SecureHandlerStateTemplate")
+	self.frame:SetResizable(false)
+	self.frame:SetClampedToScreen(true)
+	self.frame:CreateBackdrop("Transparent")
+
+	self.frame.buttons = {}
+	self:ButtonFactory()
+	self:UpdateBar(true)
+
+	E:CreateMover(self.frame, "ElvUI_RMBarMover", L["Raid Markers Bar"])
+
+	self:UpdateMover()
+end
+
+E:RegisterModule(RM:GetName())
+
+EP:RegisterPlugin(..., InjectOptions)
---
+++ ElvUI_RaidMarkers/Locales.lua
@@ -0,0 +1,128 @@
+-- English
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "enUS", true);
+
+if not L then return end
+
+L["Choose the orientation of the raid markers bar."] = true;
+L["Click to clear the mark."] = true;
+L["Click to mark the target."] = true;
+L["ElvUI Raid Markers"] = true;
+L["Orientation"] = true;
+L["Raid Markers Bar"] = true;
+L["Raid Markers"] = true;
+L["Select how the raid markers bar will be displayed."] = true;
+L["Show"] = true;
+
+if GetLocale() == "enUS" then return end
+
+-- German
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "deDE")
+if L then
+	L["Choose the orientation of the raid markers bar."] = true;
+	L["Click to clear the mark."] = true;
+	L["Click to mark the target."] = true;
+	L["ElvUI Raid Markers"] = true;
+	L["Orientation"] = true;
+	L["Raid Markers Bar"] = true;
+	L["Raid Markers"] = true;
+	L["Select how the raid markers bar will be displayed."] = true;
+	L["Show"] = true;
+end
+
+-- Spanish
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "esES")
+if L then
+	L["Choose the orientation of the raid markers bar."] = true;
+	L["Click to clear the mark."] = true;
+	L["Click to mark the target."] = true;
+	L["ElvUI Raid Markers"] = true;
+	L["Orientation"] = true;
+	L["Raid Markers Bar"] = true;
+	L["Raid Markers"] = true;
+	L["Select how the raid markers bar will be displayed."] = true;
+	L["Show"] = true;
+end
+
+-- French
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "frFR")
+if L then
+	L["Choose the orientation of the raid markers bar."] = true;
+	L["Click to clear the mark."] = true;
+	L["Click to mark the target."] = true;
+	L["ElvUI Raid Markers"] = true;
+	L["Orientation"] = true;
+	L["Raid Markers Bar"] = true;
+	L["Raid Markers"] = true;
+	L["Select how the raid markers bar will be displayed."] = true;
+	L["Show"] = true;
+end
+
+-- Korean
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "koKR")
+if L then
+	L["Choose the orientation of the raid markers bar."] = true;
+	L["Click to clear the mark."] = true;
+	L["Click to mark the target."] = true;
+	L["ElvUI Raid Markers"] = true;
+	L["Orientation"] = true;
+	L["Raid Markers Bar"] = true;
+	L["Raid Markers"] = true;
+	L["Select how the raid markers bar will be displayed."] = true;
+	L["Show"] = true;
+end
+
+-- Portuguese
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "ptBR")
+if L then
+	L["Choose the orientation of the raid markers bar."] = true;
+	L["Click to clear the mark."] = true;
+	L["Click to mark the target."] = true;
+	L["ElvUI Raid Markers"] = true;
+	L["Orientation"] = true;
+	L["Raid Markers Bar"] = true;
+	L["Raid Markers"] = true;
+	L["Select how the raid markers bar will be displayed."] = true;
+	L["Show"] = true;
+end
+
+-- Russian
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "ruRU")
+if L then
+	L["Choose the orientation of the raid markers bar."] = true;
+	L["Click to clear the mark."] = true;
+	L["Click to mark the target."] = true;
+	L["ElvUI Raid Markers"] = true;
+	L["Orientation"] = true;
+	L["Raid Markers Bar"] = true;
+	L["Raid Markers"] = true;
+	L["Select how the raid markers bar will be displayed."] = true;
+	L["Show"] = true;
+end
+
+--Chinese (China, simplified)
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "zhCN")
+if L then
+	L["Choose the orientation of the raid markers bar."] = true;
+	L["Click to clear the mark."] = true;
+	L["Click to mark the target."] = true;
+	L["ElvUI Raid Markers"] = true;
+	L["Orientation"] = true;
+	L["Raid Markers Bar"] = true;
+	L["Raid Markers"] = true;
+	L["Select how the raid markers bar will be displayed."] = true;
+	L["Show"] = true;
+end
+
+--Chinese (Taiwan, traditional)
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "zhTW")
+if L then
+	L["Choose the orientation of the raid markers bar."] = true;
+	L["Click to clear the mark."] = true;
+	L["Click to mark the target."] = true;
+	L["ElvUI Raid Markers"] = true;
+	L["Orientation"] = true;
+	L["Raid Markers Bar"] = true;
+	L["Raid Markers"] = true;
+	L["Select how the raid markers bar will be displayed."] = true;
+	L["Show"] = true;
+end
